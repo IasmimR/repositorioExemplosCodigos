1158dd9802df9de6dc6b9706c4237a1daaeedff9
Iasmim <programadoresifetrv@gmail.com>
2014-03-30 21:06:38 -0300

    Exemplo1
    Exemplo 1 - Olá Mundo

---
+++ Exemplos/src/integracao/exemplos/ExemplosServlet.java
@@ -0,0 +1,13 @@
+package integracao.exemplos;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ExemplosServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Olá Mundo");
+	}
+}
